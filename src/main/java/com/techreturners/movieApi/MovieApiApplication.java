d4b0d5aef42182d87761ad0be4ce8eeed06fc355
UDAYBHASKAR\yenne <yenneti.uday@hotmail.com>
2023-03-16 18:44:31 +0000

    Created Project for Movies Api

---
+++ src/main/java/com/techreturners/movieApi/MovieApiApplication.java
@@ -0,0 +1,13 @@
+package com.techreturners.movieApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techreturners/movieApi/MovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techreturners.movieApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
