2354cb3a0cbc1a85136a1db0ea67314f85ddc9ef
UDAYBHASKAR\yenne <yenneti.uday@hotmail.com>
2023-03-19 14:48:54 +0000

    Refactored the code for controller

--- src/main/java/com/techreturners/movieApi/controller/GenreController.java
+++ src/main/java/com/techreturners/movieApi/controller/GenreController.java
@@ -3,11 +3,11 @@ package com.techreturners.movieApi.controller;
 import com.techreturners.movieApi.service.GenreService;
 import com.techreturners.movieApi.vo.Genres;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/genres")
@@ -17,7 +17,13 @@ public class GenreController {
     GenreService genreService;
 
     @GetMapping
-    public Genres getAllGenres() throws IOException {
-        return genreService.getGenres();
+    public ResponseEntity<?> getAllGenres() {
+        Genres genres;
+        try {
+            genres = genreService.getGenres();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error Retrieving genres.");
+        }
+        return ResponseEntity.ok(genres);
     }
 }
